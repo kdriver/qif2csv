ad887ba771a2aff96871e7bf83751490f0e78f0a
keith <keith@just-the-one.co.uk>
2018-01-12 09:33:05 +0000

    Initial commit
    Converts Intuit format files ( https://en.wikipedia.org/wiki/Quicken_Interchange_Format ) to CSV for Excel import

---
+++ qif.py
@@ -0,0 +1,24 @@
+
+f = open('new.qif')
+
+contents = []
+for my_line in f:
+    if ( my_line[0]!= '!'):
+        contents.append(my_line.strip('\n'))
+
+
+record = ""
+for r in contents:
+    if r == "^":
+        print(record)
+        record = "" 
+    else:
+        if ( r != "" ):
+            sep = ','
+            if ( record == "" ):
+                sep=''
+            text = sep + r[1:]
+            record+=text 
+
+
+
